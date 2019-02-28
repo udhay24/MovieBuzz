36023c506aade375711aa33696db212efbb07859
udhay24 <udhayakumar241998@gmail.com>
2019-02-28 19:51:53 +0530

    add interceptor for okhttp client

--- app/src/main/java/com/example/moviebuzz/Repository/OkHttpClient.kt
+++ app/src/main/java/com/example/moviebuzz/Repository/OkHttpClient.kt
@@ -1,4 +1,30 @@
 package com.example.moviebuzz.Repository
 
+import okhttp3.OkHttpClient
+
 class OkHttpClient {
+
+    fun getOkHttpClient(): OkHttpClient{
+
+        return OkHttpClient.Builder()
+
+            //Add Application interceptor to add tmdb api key to all the request
+            .addInterceptor {
+
+                //build a new okhttp Url with api key
+                val newUrl = it.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("api_key" , TMDB_API_KEY)
+                    .build()
+
+                val newRequest = it.request()
+                    .newBuilder()
+                    .url(newUrl)
+                    .build()
+
+                it.proceed(newRequest)
+            }
+            .build()
+    }
 }
