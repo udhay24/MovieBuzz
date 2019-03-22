b84ff773ecc4fef9e8c6d16e6cf1bbe4c012f964
udhay24 <udhayakumar241998@gmail.com>
2019-03-22 19:37:30 +0530

    removed imports

--- app/src/main/java/com/example/moviebuzz/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviebuzz/ui/MainActivity.kt
@@ -1,16 +1,7 @@
 package com.example.moviebuzz.ui
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.example.moviebuzz.R
-import com.example.moviebuzz.ui.moviefragment.MovieFragment
 import dagger.android.DaggerActivity
 
 class MainActivity : DaggerActivity() {
