9919130d16d7bbf41eff308bc64d5d9f618fc5c8
udhay24 <udhayakumar241998@gmail.com>
2019-03-31 16:23:41 +0530

    made changes in dependency

--- app/src/main/java/com/example/moviebuzz/ui/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/moviebuzz/ui/mainactivity/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.moviebuzz.ui.mainactivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.moviebuzz.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.android.AndroidInjector
 import dagger.android.DaggerActivity
 import dagger.android.DispatchingAndroidInjector
@@ -13,12 +17,14 @@ import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
 
-    @Inject lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navController = findNavController(this, R.id.nav_controller_fragment)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        setupWithNavController(bottomNavigationView, navController)
     }
 }
