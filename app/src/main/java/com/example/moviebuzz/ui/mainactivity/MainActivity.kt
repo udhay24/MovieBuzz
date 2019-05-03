34db4a93e8672be5a6d52ac6c396ace5dd8e2f2c
udhay24 <udhayakumar241998@gmail.com>
2019-05-03 15:29:59 +0530

    hide bottom navigation when not needed

--- app/src/main/java/com/example/moviebuzz/ui/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/moviebuzz/ui/mainactivity/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.moviebuzz.ui.mainactivity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.moviebuzz.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +20,13 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         setupWithNavController(bottomNavigationView, navController)
+        if (bottomNavigationView == null) {
+            Timber.v("Bottom view is null")
+        } else {
+            Timber.v("Bottom is Not null")
+        }
+        FragmentManager.OnBackStackChangedListener {
+            Timber.v("Backstack")
+        }
     }
 }
--- app/src/main/java/com/example/moviebuzz/ui/moviedetail/MovieDetailFragment.kt
+++ app/src/main/java/com/example/moviebuzz/ui/moviedetail/MovieDetailFragment.kt
@@ -1,11 +1,15 @@
 package com.example.moviebuzz.ui.moviedetail
 
+import android.animation.Animator
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.moviebuzz.R
+import com.example.moviebuzz.ui.mainactivity.MainActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 
 class MovieDetailFragment : Fragment() {
@@ -23,4 +27,37 @@ class MovieDetailFragment : Fragment() {
         return inflater.inflate(R.layout.movie_detail_fragment, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (activity as MainActivity).bottom_navigation.apply {
+            animate()
+                .translationYBy(this.height.toFloat())
+                .setListener(object : Animator.AnimatorListener {
+                    override fun onAnimationRepeat(animation: Animator?) {}
+                    override fun onAnimationEnd(animation: Animator?) {}
+                    override fun onAnimationCancel(animation: Animator?) {}
+                    override fun onAnimationStart(animation: Animator?) {
+                        this@apply.visibility = View.GONE
+                    }
+                })
+                .duration = 200
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        (activity as MainActivity).bottom_navigation.apply {
+            animate()
+                .translationYBy(-this.height.toFloat())
+                .setListener(object : Animator.AnimatorListener {
+                    override fun onAnimationRepeat(animation: Animator?) {}
+                    override fun onAnimationEnd(animation: Animator?) {}
+                    override fun onAnimationCancel(animation: Animator?) {}
+                    override fun onAnimationStart(animation: Animator?) {
+                        this@apply.visibility = View.VISIBLE
+                    }
+                })
+                .duration = 200
+        }
+    }
 }
